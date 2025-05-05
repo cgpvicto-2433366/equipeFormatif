647b8a0a459df08195c7b14ccce79c729579e127
schalom16 <2433366@carrefour.cegepvicto.ca>
2025-05-05 17:10:08 -0400

    Initialisation du projet

---
+++ equipe/Postman/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Postman
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Projet principale gogogogog
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
